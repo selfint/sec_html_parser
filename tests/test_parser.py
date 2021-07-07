ea4ce8fe4f3c769d694f22652e3c08dfb4f77478
Tom Selfin <selfint@gmail.com>
2021-07-07 09:25:02 +0300

    rename test to correct name

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ from sec_html_parser.parser import Parser
 
 
 class TestParser(unittest.TestCase):
-    def test_text_is_child(self):
+    def test_parse_simple(self):
         html = """
     <div style="margin-top:9pt;text-align:justify"><span style="color:#000000;font-family:'Helvetica',sans-serif;font-size:9pt;font-weight:700;line-height:120%">Company Background</span></div>
     <div style="margin-top:6pt;text-align:justify"><span style="color:#000000;font-family:'Helvetica',sans-serif;font-size:9pt;font-weight:400;line-height:120%">The Company designs, manufactures and markets smartphones.</span></div>
