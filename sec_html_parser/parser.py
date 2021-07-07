9a616d55cef3ddf478664f3ea39425d2ce857964
Tom Selfin <selfint@gmail.com>
2021-07-07 18:45:26 +0300

    implement is_child

--- sec_html_parser/parser.py
+++ sec_html_parser/parser.py
@@ -1,2 +1,41 @@
+import bs4.element
+from bs4 import BeautifulSoup
+
+from sec_html_parser.font_style import FontStyle
+
+
 class Parser:
-    pass
+    def _is_child(self, node: bs4.element.Tag, other: bs4.element.Tag) -> bool:
+        """Check if node is a child of other with respect to font styles"""
+
+        # check that other has a style
+        try:
+            ostyle = FontStyle(other)
+        except ValueError:
+            return False
+
+        # a node with no style is always a child
+        try:
+            nstyle = FontStyle(node)
+        except ValueError:
+            return True
+
+        # check if style by size
+        if ostyle.size is not None:
+            if nstyle.size is not None and nstyle.size < ostyle.size:
+                return True
+
+        # check if style by weight
+        if ostyle.weight is not None:
+            if nstyle.weight is not None and nstyle.weight < ostyle.weight:
+                return True
+
+        # child if style by style
+        if ostyle.style is not None:
+            if nstyle.style is not None:
+                if ostyle.style == "italic" and nstyle.style != "italic":
+                    return True
+            return ostyle.style == "italic"
+
+        # if not child by any of the above then it is not a child
+        return False
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,20 @@
 import unittest
 
+from bs4 import BeautifulSoup
+
 from sec_html_parser.parser import Parser
 
 
 class TestParser(unittest.TestCase):
     def test_parser_creation(self):
         p = Parser()
+
+    def test_is_child_size_larger(self):
+        child = BeautifulSoup('<span style="font-size:9pt"', features="html.parser")
+        child = list(child.children)[0]
+        parent = BeautifulSoup('<span style="font-size:10pt"', features="html.parser")
+        parent = list(parent.children)[0]
+
+        p = Parser()
+        self.assertTrue(p._is_child(child, parent))
+        self.assertFalse(p._is_child(parent, child))
