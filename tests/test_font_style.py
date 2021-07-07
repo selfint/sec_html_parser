4f2b4a4c375850a6758fd43f63138ce2739d901c
Tom Selfin <selfint@gmail.com>
2021-07-07 19:04:19 +0300

    add font-style extraction test to FontStyle from str test

--- tests/test_font_style.py
+++ tests/test_font_style.py
@@ -7,11 +7,11 @@ from sec_html_parser.font_style import FontStyle
 
 class TestFontStyle(unittest.TestCase):
     def test_font_style(self):
-        span_style = "color:#000000;font-family:'Helvetica',sans-serif;font-size:9pt;font-weight:400;line-height:120%"
+        span_style = "color:#000000;font-family:'Helvetica',sans-serif;font-size:9pt;font-style:italic;font-weight:400;line-height:120%"
         font_style = FontStyle(span_style)
         self.assertEqual(font_style.size, 9)
         self.assertEqual(font_style.weight, 400)
-        self.assertEqual(font_style.style, None)
+        self.assertEqual(font_style.style, "italic")
 
     def test_font_style_from_node(self):
         soup = BeautifulSoup(
