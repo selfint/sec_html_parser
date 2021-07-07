8b4ba6c3ac0e8e61a1a80df2c473a80949ea3161
Tom Selfin <selfint@gmail.com>
2021-07-07 18:36:10 +0300

    implement FontStyle from BeautifulSoup node

--- sec_html_parser/font_style.py
+++ sec_html_parser/font_style.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 
 import regex as re
+from bs4 import BeautifulSoup
 
 
 @dataclass
@@ -12,10 +13,12 @@ class FontStyle:
     weight: Optional[int]
     style: Optional[str]
 
-    def __init__(self, span_style: str) -> None:
+    def __init__(self, node_or_style: Union[str, BeautifulSoup]) -> None:
         _font_size_re = re.compile("font-size:(\d+)")
         _font_weight_re = re.compile("font-weight:(\d+)")
         _font_style_re = re.compile("font-style:([a-zA-Z]+)")
+
+        span_style = self._get_style_string(node_or_style)
 
         size = _font_size_re.search(span_style)
         self.size = None if size is None else int(size.group(1))
@@ -25,3 +28,21 @@ class FontStyle:
 
         style = _font_style_re.search(span_style)
         self.style = None if style is None else style.group(1)
+
+    @staticmethod
+    def _get_style_string(node_or_style: Union[str, BeautifulSoup]) -> str:
+        """Get a style string from a string or a bs4 Node"""
+
+        if isinstance(node_or_style, str):
+            return node_or_style
+        elif isinstance(node_or_style, BeautifulSoup):
+            node_style = node_or_style.get("style")
+            if node_style is not None:
+                return node_style
+            else:
+                raise ValueError("Given node didn't have a 'style' attribute")
+        else:
+            raise ValueError(
+                "Can't create FontStyle from object"
+                f"of type '{node_or_style.__class__.__name__}'"
+            )
--- tests/test_font_style.py
+++ tests/test_font_style.py
@@ -1,5 +1,7 @@
 import unittest
 
+from bs4 import BeautifulSoup
+
 from sec_html_parser.font_style import FontStyle
 
 
@@ -9,4 +11,14 @@ class TestFontStyle(unittest.TestCase):
         font_style = FontStyle(span_style)
         self.assertEqual(font_style.size, 9)
         self.assertEqual(font_style.weight, 400)
+        self.assertEqual(font_style.style, None)
+
+    def test_font_style_from_node(self):
+        soup = BeautifulSoup(
+            '<span style="font-size:9pt></span>"', features="html.parser"
+        )
+        span_node = list(soup.children)[0]
+        font_style = FontStyle(span_node)
+        self.assertEqual(font_style.size, 9)
+        self.assertEqual(font_style.weight, None)
         self.assertEqual(font_style.style, None)
